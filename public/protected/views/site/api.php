a01ff26f2b5894a13d64678b197fa11e718b7200
Mike Rogers <mike.r@fullondesign.co.uk>
2013-02-20 12:00:15 +0000

    updating api

--- public/protected/views/site/api.php
+++ public/protected/views/site/api.php
@@ -14,5 +14,36 @@ $this->pageTitle=Yii::app()->name;
 		<h1>API</h1>
 		<p>This application allows you to process javascript files in the cloud via an API</p>
 		<p>TODO - Document</p>
+		<h2>Staring your crunch</h2>
+		<code>
+			<?php
+				echo htmlentities("
+			self.addEventListener('message', function(e) {
+				if(e.data.cmd == 'start'){
+					cruncher.data = e.data.data;
+					self.postMessage({'cmd': 'log','data': 'Starting'});
+					cruncher.run();
+				}
+			}, false);
+			");
+			?> 
+		</code>
+		<h2>Save a crunch</h2>
+		<code>
+			<?php
+				echo htmlentities("
+			self.postMessage({'cmd': 'completed','data': this.results});
+			");
+			?> 
+		</code>
+		
+		<h2>Log data while crunching</h2>
+		<code>
+			<?php
+				echo htmlentities("
+			self.postMessage({'cmd': 'log','data': this.data});
+			");
+			?> 
+		</code>
 	</div>
 </div>
