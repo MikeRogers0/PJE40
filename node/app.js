597793ba9fafba15c423c4d6a5d3d2e9c37ebae2
Mike Rogers <mike.r@fullondesign.co.uk>
2013-03-27 20:24:31 +0000

    adding in packages for node

---
+++ node/app.js
@@ -0,0 +1,5 @@
+// Set up the requires
+var 
+	// MySQL is the DB on the server. Library is https://github.com/felixge/node-mysql 
+	mysql = require('mysql'),
+	socket = requre('socket'); // And talk to the client via a socket
