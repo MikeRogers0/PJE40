3b1f9ff1b75ff58af1f3bcb1b67555135fa7eaad
Mike Rogers <mike.r@fullondesign.co.uk>
2013-04-07 16:15:15 +0100

    adding in socket.io stuff

--- node/app.js
+++ node/app.js
@@ -2,4 +2,44 @@
 var 
 	// MySQL is the DB on the server. Library is https://github.com/felixge/node-mysql 
 	mysql = require('mysql'),
-	socket = requre('socket'); // And talk to the client via a socket
+	// The Socket.io libary from http://socket.io/
+	io = require('socket.io').listen(1337),
+	// List of users ready for tests
+	idleUsers = [],
+	activeUsers = [];
+
+io.sockets.on('connection', function (socket) {  
+	// When the user is ready to recieve a test
+	socket.on('ready', function () {
+		idleUsers[socket] = true;
+	});
+	
+	socket.on('save', function () {
+		// Save the data the user was working on.
+		if(activeUsers[socket].task == true){
+			console.log();
+		}
+		
+		// Set the user as idle.
+		delete activeUsers[socket];
+		idleUsers[socket] = true;
+	});
+	
+	// When the user disconnects remove them from the list
+	socket.on('disconnect', function () {
+		// If a user was working on something, mark it as failed.
+		if(activeUsers[socket].task == true){
+			// mark it as failed
+			
+			delete activeUsers[socket];
+		}
+		
+		// If the user is not working on anything and leaves, it's ok.
+		if(idleUsers[socket] === true){
+			delete idleUsers[socket];
+			return;
+		}
+	});
+});
+
+// Send a user a task.
